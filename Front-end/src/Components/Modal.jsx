dd881ed697dec6421c688c4f3ec46969a526a264
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-11-10 12:15:52 +0500

    fix: update product creation API URL to use test environment

--- Front-end/src/Components/Modal.jsx
+++ Front-end/src/Components/Modal.jsx
@@ -38,8 +38,10 @@ const AddProduct = () => {
 
       console.log(formData);
 
+      // https://ebooks-store.onrender.com
+
       const response = await axios.post(
-        "https://ebooks-store.onrender.com/products/createProduct",
+        "https://ebooks-store-fileupload-feature-test.onrender.com/products/createProduct",
         formDataToSend,
         {
           headers: {
